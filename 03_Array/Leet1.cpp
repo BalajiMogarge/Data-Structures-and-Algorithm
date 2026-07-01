f123b3bc7900e26a9d804e1fa1481c28ff1e5ffc
BalajiMogarge <balajimogarge94@gmail.com>
2026-07-01 23:52:01 +0530

    Leet1

---
+++ 03_Array/Leet1.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> seen;
+        for (int i = 0; i < nums.size(); ++i) {
+            int complement = target - nums[i];
+            if (seen.count(complement)) {
+                return {seen[complement], i};
+            }
+            seen[nums[i]] = i;
+        }
+        return {};
+    }
+};
+int main() {
+    Solution solution;
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    vector<int> result = solution.twoSum(nums, target);
+    if (!result.empty()) {
+        cout << "[" << result[0] << ", " << result[1] << "]" << endl;
+    }
+    return 0;
+}
